94e546a48a3672de4fa0a234eb39e1a7a660741b
Travis-CI <travis@travis-ci.org>
2016-03-06 15:22:07 +0000

    Bump version: 0.0.21 -> 0.0.22
    [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if platform.system() != "Windows":
     ])
 
 setup(name="polysquare-travis-container",
-      version="0.0.21",
+      version="0.0.22",
       description="""Polysquare Travis-CI Container Root""",
       long_description_markdown_filename="README.md",
       author="Sam Spilsbury",
