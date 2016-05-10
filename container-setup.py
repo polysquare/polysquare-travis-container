dc1dbed7002885fd32c3770efc3780f1e24348a0
Sam Spilsbury <smspillaz@gmail.com>
2016-05-10 16:42:23 +0800

    container-setup: Pre-download files before running tests

--- container-setup.py
+++ container-setup.py
@@ -8,6 +8,8 @@
 # See /LICENCE.md for Copyright information
 """Initial setup script specific to polysquare-ci-scripts."""
 
+import os
+
 
 def run(cont, util, shell, argv=list()):
     """Set up language runtimes and pass control to python project script."""
@@ -18,3 +20,18 @@ def run(cont, util, shell, argv=list()):
     shell.overwrite_environment_variable(cache_dir_key, cache_dir)
 
     cont.fetch_and_import("setup/python/setup.py").run(cont, util, shell, argv)
+
+    config_python = "setup/project/configure_python.py"
+    py_ver = util.language_version("python3")
+    py_cont = cont.fetch_and_import(config_python).get(cont,
+                                                       util,
+                                                       shell,
+                                                       py_ver)
+
+    with py_cont.activated(util):
+        with util.Task("""Downloading all distributions"""):
+            os.environ[cache_dir_key] = cache_dir
+            util.execute(cont,
+                         util.long_running_suppressed_output(),
+                         util.which("python"),
+                         "download-all-distros-to.py")
