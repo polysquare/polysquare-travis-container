4ea6683a30e9e5d47db2519daeada45abe9ddf96
Sam Spilsbury <smspillaz@gmail.com>
2016-05-30 16:09:07 +0800

    debian: Handle data.tar.xz too

--- psqtraviscontainer/debian_package.py
+++ psqtraviscontainer/debian_package.py
@@ -15,7 +15,19 @@ def extract_deb_data(archive, extract_dir):
     # We may not have python-debian installed on all platforms
     from debian import arfile  # suppress(import-error)
 
-    with closing(arfile.ArFile(archive).getmember("data.tar.gz")) as member:
-        with tarfile.open(fileobj=member,
-                          mode="r|*") as data_tar:
-            data_tar.extractall(path=extract_dir)
+    data_members = ["data.tar.gz", "data.tar.xz"]
+
+    for data_mem in data_members:
+        try:
+            with closing(arfile.ArFile(archive).getmember(data_mem)) as member:
+                with tarfile.open(fileobj=member,
+                                  mode="r|*") as data_tar:
+                    data_tar.extractall(path=extract_dir)
+
+            # Succeeded, break out here
+            break
+        except KeyError as error:
+            if str(error) == data_mem:
+                continue
+            else:
+                raise error
