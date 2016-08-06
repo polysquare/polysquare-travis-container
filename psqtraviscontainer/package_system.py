88949ac15968d4a6cbf568fcf88940c08d4a956e
Sam Spilsbury <smspillaz@gmail.com>
2016-08-06 18:48:07 +0800

    package_system: Refactor out function to format apt repositories

--- psqtraviscontainer/package_system.py
+++ psqtraviscontainer/package_system.py
@@ -90,15 +90,29 @@ class Dpkg(PackageSystem):
         self._arch = arch
         self._executor = executor
 
-    def add_repositories(self, repos):
-        """Add a repository to the central packaging system."""
+    @staticmethod
+    def format_repositories(repos, release, arch):
+        """Take a list of APT lines and format them.
+
+        There are certain shortcuts that you can use.
+
+        {ubuntu} will be replaced by http://archive.ubuntu.com/ and
+        the architecture.
+
+        {debian} will be replaced by http://ftp.debian.org/.
+
+        {launchpad} will be replaced by "http://ppa.launchpad.net/.
+
+        {release} gets replaced by the release of the distribution, which
+        means you don't need a repository file for every distribution.
+        """
         _ubuntu_urls = [
             (_UBUNTU_MAIN_ARCHS, _UBUNTU_MAIN_ARCHIVE),
             (_UBUNTU_PORT_ARCHS, _UBUNTU_PORT_ARCHIVE)
         ]
 
         def _format_user_line(line, kwargs):
-            """Format a line and turns it into a valid repo line."""
+            """Format a line and turns it into a valid repository line."""
             formatted_line = line.format(**kwargs)
             return "deb {0}".format(formatted_line)
 
@@ -107,21 +121,27 @@ class Dpkg(PackageSystem):
             return value[0] if len(value) else "ERROR"
 
         format_keys = {
-            "ubuntu": [u[1] for u in _ubuntu_urls if self._arch in u[0]],
+            "ubuntu": [u[1] for u in _ubuntu_urls if arch in u[0]],
             "debian": ["http://ftp.debian.org/"],
             "launchpad": ["http://ppa.launchpad.net/"],
-            "release": [self._release]
+            "release": [release]
         }
         format_keys = {
             k: _value_or_error(v) for k, v in format_keys.items()
         }
 
+        return [_format_user_line(l, format_keys) for l in repos]
+
+    def add_repositories(self, repos):
+        """Add a repository to the central packaging system."""
         # We will be creating a bash script each time we need to add
         # a new source line to our sources list and executing that inside
         # the proot. This guarantees that we'll always get the right
         # permissions.
         with tempfile.NamedTemporaryFile() as bash_script:
-            append_lines = [_format_user_line(l, format_keys) for l in repos]
+            append_lines = Dpkg.format_repositories(repos,
+                                                    self._release,
+                                                    self._arch)
             for count, append_line in enumerate(append_lines):
                 path = "/etc/apt/sources.list.d/{0}.list".format(count)
                 append_cmd = "echo \"{0}\" > {1}\n".format(append_line, path)
