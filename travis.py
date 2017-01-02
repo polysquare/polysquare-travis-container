0bad577e8f198c63449ea092daeae52e4a103416
Sam Spilsbury <smspillaz@gmail.com>
2017-01-02 17:59:51 +0800

    try doing it through bash

--- travis.py
+++ travis.py
@@ -2,4 +2,4 @@
 
 import subprocess
 
-print(subprocess.check_call(["psq-travis-container-create", "--distro=Ubuntu", "--release=precise", "--arch=x86_64", "--local", "container"]));
+print(subprocess.check_call(["bash", "-c",  "psq-travis-container-create --distro=Ubuntu --release=precise --arch=x86_64 --local container"]));
