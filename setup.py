7cbb926dc3a3b08e47b6cf52fa5851e7db7a4bbc
Sam Spilsbury <smspillaz@gmail.com>
2021-02-02 00:29:38 +0200

    setup: Bump version to 0.0.46

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if platform.system() != "Windows":
     ])
 
 setup(name="polysquare-travis-container",
-      version="0.0.45",
+      version="0.0.46",
       description="""Polysquare Travis-CI Container Root""",
       long_description_markdown_filename="README.md",
       author="Sam Spilsbury",
